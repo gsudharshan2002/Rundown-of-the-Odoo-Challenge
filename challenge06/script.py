7cbacc8087887071efb78a56f0f81eb37c81235e
G Sudharshan <140174934+gsudharshan2002@users.noreply.github.com>
2025-01-03 16:19:40 +0530

    Update script.py

--- challenge06/script.py
+++ challenge06/script.py
@@ -1,6 +1,6 @@
 
 idx = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-tmp = [5, 38, 36, 3, 3, 0, 0, 4, 3, 14, 37, 38, 40, 5, 9, 11, 1, 5, 3, 5]
+tmp = [0, 6, 38, 2, 36, 14, 8, 3, 2, 15, 1, 5, 7, 36, 41, 14, 9, 12, 3, 14]
 pwd = []
 for i in tmp:
     pwd.append(idx[i])
